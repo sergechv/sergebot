ff72957050d258297b87db1efb92a04d299a9fc0
Sergio Chaidez <sergioj.chaidez@gmail.com>
2024-07-10 22:20:07 -0600

    deploying commands

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ client.on(Events.InteractionCreate, interaction => {
   }
 
   try {
-    await command.execute(interaction);
+    command.execute(interaction);
   } catch (error) {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: `There was an error while executing this command!`, ephemeral: true });
+      interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
     } else {
-      await interaction.reply({ content: `There was an error while executing this command!`, ephemeral: true });
+      interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
   }
 });
